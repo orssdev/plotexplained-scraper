70b40661458ce45d2fd43d2fb8a2bf413ab033ad
Oscar <orss4123@gmail.com>
2025-09-24 13:06:58 -0400

    items from info

--- plot_explained/plot_explained/spiders/plot_explained_spider.py
+++ plot_explained/plot_explained/spiders/plot_explained_spider.py
@@ -7,7 +7,7 @@ class PlotExplainedSpider(scrapy.Spider):
     async def start(self):
         start_year = 2015
         end_year = 2025
-        pages = 5
+        pages = 2
         urls = [f'https://www.plotexplained.com/movie?sort=latest-release&fromYear={start_year}&toYear={end_year}&page={x}' for x in range(1, pages + 1)]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -23,9 +23,41 @@ class PlotExplainedSpider(scrapy.Spider):
     def movie_parse(self, response):
         title = response.css('h1::text').get()
         info = response.css('div.flex.pt-4.flex-row.font-semibold.text-base.sm\\:text-lg.xl\\:text-xl.font-source-pro.gap-x-8.md\\:gap-x-14.gap-y-2.sm\\:gap-y-3.md\\:gap-y-4.flex-wrap p::text').getall()
+        capitalize = response.css('span.capitalize::text').getall()
+        desc = response.css('p.text-base.lg\\:text-lg.text-foreground::text').get()
+        year = 'No year listed'
+        runtime = 'No runtime listed'
+        language = 'No language listed'
+        director = 'No director(s) listed'
+        budget = 'No budget listed'
+        echo_score = 'No echo score listed'
+        for item in info:
+            if item != ' ':
+                data = item.split()
+                item = data[0]
+                if item == 'Year:':
+                    year = data[1]
+                elif item == 'Runtime:':
+                    runtime = ' '.join(data[1:])
+                elif item == 'Language:':
+                    language = capitalize[0].title()
+                elif item == 'Director:':
+                    director = ' '.join(data[1:])
+                elif item == 'Directors:':
+                    director = (' '.join(data[1:])).split(',')
+                elif item == 'Budget:':
+                    budget = data[1]
+            
+
         yield {
             'url': response.url,
             'title': title,
-            'info': info
+            'year': year,
+            'runtime': runtime,
+            'language' : language,
+            'director' : director,
+            'budget' : budget,
+            'genre': capitalize[1:],
+            'description': desc
         }
     
